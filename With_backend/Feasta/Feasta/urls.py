606f2a5ced10be76042fd6c33c32d3d7d0821b95
Disha Pratikkumar Shah <dpshah269@gmail.com>
2020-11-28 22:53:18 +0530

    Added User and Admin side pages

--- With_backend/Feasta/Feasta/urls.py
+++ With_backend/Feasta/Feasta/urls.py
@@ -21,7 +21,19 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name = "admin"),
-    path('Admin-Menu/',lv.index, name="Admin-Menu")
+    path('register/', lv.Register, name="register"),
+    path('login/', lv.Login, name="login"),
+    path('', lv.Home, name="home"),
+    path('about/', lv.About, name="about"),
+    path('blog/', lv.Blog, name="blog"),
+    path('blog-details/', lv.BlogDetails, name="blog-details"),
+    path('contact/', lv.Contact, name="contact"),
+    path('cart/', lv.Cart, name="cart"),
+    path('admin-menu/',lv.index, name="Admin-Menu"),
+    path('menu/',lv.menu, name="menu"),
+    path('add-item/',lv.Add_Item, name="Add-Item"),
+    path('dashboard/',lv.Admin_Index, name="Admin-Index"),
+    path('add-category/',lv.Add_Category, name="Add-Category")
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- With_backend/Feasta/login/views.py
+++ With_backend/Feasta/login/views.py
@@ -29,7 +29,46 @@ def add_item_form_submission(request):
     Product_info.save()
     return render(request , 'Add-Item.html')'''
 
+def Register(request):
+    return render(request, 'register.html')
+
+def Login(request):
+    return render(request, 'login.html')
+
+def Home(request):
+    return render(request, 'index.html')
+
+def About(request):
+    return render(request, 'about.html')
+
+def Blog(request):
+    return render(request, 'blog.html')
+
+def BlogDetails(request):
+    return render(request, 'blog-details.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
+
+def Cart(request):
+    return render(request, 'Test.html')
+
 def index(request):
     dests = user_info.objects.all()
     print(dests)
     return render(request, "Admin-Menu.html", {'dests':dests})
+
+def menu(request):
+    dests = user_info.objects.all()
+    print(dests)
+    return render(request, "menu.html", {'dests':dests})
+
+def Add_Item(request):
+    return render(request, 'Add-Item.html')
+
+def Add_Category(request):
+    return render(request, 'Add-category.html')
+
+def Admin_Index(request):
+    return render(request, 'Admin-index.html')
+
